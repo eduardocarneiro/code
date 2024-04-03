2ff58cf7607138c923c7d81245d9547c54a66e10
Eduardo Carneiro <eduardo.oc@gmail.comq!>
2024-04-03 11:30:16 -0300

    aula 5.8 pacotes - como criar pacotes e organizar as classes

---
+++ java/training/algaworks_java_e_orientacao_a_objetos/5-orientacao-a-objetos-parte2/5.8-organizando-os-projetos-em-pacotes/Cachorro.java
@@ -0,0 +1,15 @@
+package lab.eoc.fjoo.animal;
+
+public class Cachorro {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
---
+++ java/training/algaworks_java_e_orientacao_a_objetos/5-orientacao-a-objetos-parte2/5.8-organizando-os-projetos-em-pacotes/TreinadorCachorro.java
@@ -0,0 +1,14 @@
+package lab.eoc.fjoo.treinador;
+
+import lab.eoc.fjoo.animal.Cachorro;
+
+public class TreinadorCachorro {
+
+	public static void main(String[] args) {
+		
+		Cachorro cachorro = new Cachorro();
+		cachorro.setNome("Natal");
+		
+	}
+
+}
