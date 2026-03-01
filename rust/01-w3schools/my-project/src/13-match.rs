01e8ce65633a4c441e26744bffabe693c8c3b768
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-02-28 22:34:29 -0300

    added multiple matches

--- rust/01-w3schools/my-project/src/13-match.rs
+++ rust/01-w3schools/my-project/src/13-match.rs
@@ -4,11 +4,12 @@ When you have many choices, using "match" is easier than writing lots of "if..el
 "match" is used to select one of many code blocks to be executed.
 */
 
-/*
-Example Explained:
-https://www.w3schools.com/rust/rust_match.php
-*/
+
 fn main() {
+    /*
+    Example Explained:
+    https://www.w3schools.com/rust/rust_match.php
+    */
     let day = 4;
 
     match day {
@@ -21,4 +22,14 @@ fn main() {
         7 => println!("Sunday"),
         _ => println!("Invalid day."),
     }
+
+    // Multiple Matches
+    let new_day = 6;
+
+    match new_day {
+        1 | 2 | 3| 4 | 5 => println!("Weekday"),
+        6 | 7 => println!("Weekend"),
+        _ => println!("Invalid day"),
+    }
+
 }
