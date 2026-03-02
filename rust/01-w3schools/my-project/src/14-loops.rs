e68299254dea4738ffdd7007798cb3c627757113
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-01 22:47:25 -0300

    added loop simplest way to use it

---
+++ rust/01-w3schools/my-project/src/14-loops.rs
@@ -0,0 +1,18 @@
+// Loops
+/*
+Rust has three types of loops: "loop", "while", and "for"
+*/
+
+fn main() {
+    // loop
+    /*
+    "loop" is the simplest of Rust's three loop types
+    It will run forever unless you tell it to stop
+    */
+    
+    loop {
+        println!("This will repeat forever!");
+    }
+    
+}
+
