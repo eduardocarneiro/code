7948bdeefaaeb4cea5f53911fe9c53e4cdf7f801
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-01 22:48:26 -0300

    added loop with break

--- rust/01-w3schools/my-project/src/14-loops.rs
+++ rust/01-w3schools/my-project/src/14-loops.rs
@@ -9,10 +9,22 @@ fn main() {
     "loop" is the simplest of Rust's three loop types
     It will run forever unless you tell it to stop
     */
-    
+    /*
     loop {
         println!("This will repeat forever!");
     }
-    
+    */
+
+    // loop break
+    let mut count = 1;
+    // to stop "loop", use the "break" keyword
+    loop {
+        println!("Hello world!");
+
+        if count == 3 {
+            break;
+        }
+        count += 1;
+    }
 }
 
