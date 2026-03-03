9536345e6c9d9c827c0ef51dab9ed05efd5be73b
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-02 21:41:29 -0300

    added while loop

---
+++ rust/01-w3schools/my-project/src/15-while-loops.rs
@@ -0,0 +1,13 @@
+// While Loop
+/*
+The "while" loop runs as long as a condition is "true"
+*/
+
+fn main() {
+    let mut count = 1;
+
+    while count <= 5 {
+        println!("Count: {}", count);
+        count += 1;
+    }
+}
