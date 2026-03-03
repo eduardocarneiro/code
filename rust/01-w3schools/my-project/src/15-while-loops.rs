90cad71234266e760ee3b636521f5848bc3502ff
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-02 21:47:11 -0300

    added while loop break

--- rust/01-w3schools/my-project/src/15-while-loops.rs
+++ rust/01-w3schools/my-project/src/15-while-loops.rs
@@ -1,6 +1,8 @@
 // While Loop
 /*
 The "while" loop runs as long as a condition is "true"
+
+The "while" loop checls the condition before each loop, so if the condition is "false" at the start, the loop will not run at all
 */
 
 fn main() {
@@ -9,5 +11,18 @@ fn main() {
     while count <= 5 {
         println!("Count: {}", count);
         count += 1;
+    }
+    // Stop a While Loop
+    /*
+    You can stop a "while" loop when you want by using "break"
+    */
+    let mut num = 1;
+
+    while num <= 10 {
+        if num == 7 {
+            break;
+        }
+        println!("Number is: {}", num);
+        num += 1;
     }
 }
