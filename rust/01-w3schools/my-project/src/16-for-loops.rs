82af0786f08ca78859791d8b772c3649d9bd197c
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-03 23:07:15 -0300

    added for loops

---
+++ rust/01-w3schools/my-project/src/16-for-loops.rs
@@ -0,0 +1,12 @@
+// "for" loop
+/*
+Use "for" when you know exaclty how many times you want to loop
+Use "for" loop together with the "in" keyword, instead of a "while" loop
+*/
+
+fn main() {
+    // This prints numbers from 1 to 5.
+    for i in 1..6 {
+        println!("i is: {}", i);
+    }
+}
