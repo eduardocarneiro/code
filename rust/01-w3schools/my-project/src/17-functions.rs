562cc098b5978ffc57a9c50b86ece2869d24e2ee
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-07 23:40:55 -0300

    added function with return values

--- rust/01-w3schools/my-project/src/17-functions.rs
+++ rust/01-w3schools/my-project/src/17-functions.rs
@@ -19,6 +19,7 @@ fn function_name() {
 fn say_hello() {
     println!("Hello from a function!");
 }
+// end
 
 // Function with Parameters
 /*
@@ -28,8 +29,24 @@ You can send information into a function using parameters
 fn greet(name: &str) {
     println!("Hello, {}!", name );
 }
+// end
+
+// Function with return values
+/*
+ - A function can return a value
+ - Use the "->" symbol in the function header to show what type of value will be returned 
+ - Inside the function, use the "return" keyword to send the value back
+*/
+fn do_sum(a: i32, b: i32) -> i32 {
+    return a + b;
+}
 
 fn main () {
     say_hello(); 
     greet("Dona Saroca!!!");
+    
+    // sample "Function with return values"
+    let sum = do_sum(1, 3);
+    println!("Sum is: {}", sum);
+    // end
 }
