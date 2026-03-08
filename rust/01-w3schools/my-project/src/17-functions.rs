30a4d86eeaf028ea9f318516d2d0756403dea1b4
Eduardo Carneiro <eduardo.oc@gmail.com>
2026-03-07 23:28:51 -0300

    added functions

---
+++ rust/01-w3schools/my-project/src/17-functions.rs
@@ -0,0 +1,25 @@
+// Functions
+/*
+- A function is a block of code that only runs when you call it
+- Functions is used to organize your code, avoid repeating yoursef, and make your program easier to understand 
+*/
+
+// creating a function
+/*
+fn function_name() {
+    // code here
+}
+*/
+
+// calling a function
+/*
+ - to call a function, write the name of the function followed by two parantheses().
+*/
+// sample
+fn say_hello() {
+    println!("Hello from a function!");
+}
+
+fn main () {
+    say_hello(); 
+}
